f85a42da784762d657a4eb301e6ca0d576cc9f43
Mario Muñoz <j.mario.msx@gmail.com>
2024-11-28 19:50:44 +0100

    modify styles

--- src/app/(dashboard)/boards/[id]/page.tsx
+++ src/app/(dashboard)/boards/[id]/page.tsx
@@ -12,22 +12,20 @@ export default async function BoardPage({ params }: { params: Promise<{ id: stri
   const taskLists = await fetchTaskLists(boardId);
 
   return (
-    <div className="relative h-full">
-      <ul className="scrollbar-transparent flex h-[calc(100%-8px)] gap-4 overflow-x-auto p-3">
-        {taskLists.map(({ id, name, tasks }) => (
-          <li className="h-full" key={id}>
-            <TaskList listName={name} tasks={tasks} />
-          </li>
-        ))}
-        <li>
-          <button
-            type="button"
-            className="flex w-[272px] items-center gap-2 rounded-xl bg-white bg-opacity-10 p-3 text-sm text-primary text-white shadow hover:bg-opacity-15">
-            <PlusIcon width={16} height={16} />
-            {taskLists.length ? 'Add another list' : 'Add a list'}
-          </button>
+    <ul className="scrollbar-transparent flex h-[calc(100%-8px)] gap-4 overflow-x-auto p-3">
+      {taskLists.map(({ id, name, tasks }) => (
+        <li className="h-full" key={id}>
+          <TaskList listName={name} tasks={tasks} />
         </li>
-      </ul>
-    </div>
+      ))}
+      <li>
+        <button
+          type="button"
+          className="flex w-[272px] items-center gap-2 rounded-xl bg-white bg-opacity-10 p-3 text-sm text-primary text-white shadow hover:bg-opacity-15">
+          <PlusIcon width={16} height={16} />
+          {taskLists.length ? 'Add another list' : 'Add a list'}
+        </button>
+      </li>
+    </ul>
   );
 }
