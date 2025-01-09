eb9fe56d3feb00f0530c195eced37318a30b6bed
Mario Muñoz <j.mario.msx@gmail.com>
2025-01-09 18:34:14 +0100

    checkpoint

--- src/components/dashboard/boards/cards.tsx
+++ src/components/dashboard/boards/cards.tsx
@@ -3,9 +3,19 @@
 import { CardList as CardListType } from '@/types/app-types';
 import { useEffect, useState } from 'react';
 import { CardList } from '@/components/dashboard/boards/card-list';
+import invariant from 'tiny-invariant';
 
 export function Cards({ initialCardLists }: { initialCardLists: CardListType[] }) {
   const [cardLists, setCardLists] = useState<CardListType[]>(initialCardLists);
+
+  useEffect(() => {
+    const el = ref.current;
+    invariant(el);
+
+    return draggable({
+      element: el,
+    });
+  }, []);
 
   useEffect(() => {
     setCardLists(initialCardLists);
